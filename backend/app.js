d78b1c552c2948300230824773d767fb5b2fbc97
Connor Lam <connorlam03@yahoo.com>
2022-07-27 15:30:03 -0700

    add error handlers

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ const isProduction = environment === 'production';
 
 const app = express();
 const routes = require('./routes');
+const {ValidationError} = require('sequelize')
 
 app.use(morgan('dev'));
 
@@ -42,7 +43,36 @@ app.use(
     )
     
     
-    app.use(routes)  //should this be somewhere specific
+app.use(routes)  //should this be somewhere specific
+
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+//process sequelize erros
+app.use((err, _req, _res, next) => {
+    //check if error is a Sequelize error
+    if (err instanceof ValidationError){
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error'
+    }
+    next(err)
+})
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    })
+})
 
 
 module.exports = app
