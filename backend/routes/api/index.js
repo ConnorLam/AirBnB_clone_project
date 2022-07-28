505eed807ff69b3813ab2d967ee31eef840f597f
Connor Lam <connorlam03@yahoo.com>
2022-07-28 16:05:02 -0700

    stuck on validateLogin with login route error

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 // const { setTokenCookie } = require('../../utils/auth.js');
 // const { User } = require('../../db/models')
-const sessionRouter = require('./session');
-const usersRouter = require('./users')
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js')
 const {restoreUser} = require('../../utils/auth.js')
 
 router.use(restoreUser)
