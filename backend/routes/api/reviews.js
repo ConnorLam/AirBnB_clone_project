07ff009fb35a6bcf752308e05295ae122ae3e03c
Connor Lam <connorlam03@yahoo.com>
2022-08-04 13:47:43 -0700

    finished added a iamge to review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -60,6 +60,49 @@ router.get('/current', requireAuth, async(req, res) => {
     res.json({Reviews: review})
 })
 
+router.post('/:reviewId/images', requireAuth, async (req, res) => {
+    const {user} = req
+    const {url, previewImage} = req.body
+    const review = await Review.findByPk(req.params.reviewId)
+    const images = await Image.findAll({
+        where: {reviewId: req.params.reviewId}
+    })
+
+
+    if(!review){
+        res.statusCode = 404
+        return res.json({
+          message: "Review couldn't be found",
+          statusCode: 404,
+        });
+    }
+
+    if(images.length >= 10){
+        res.statusCode = 403
+        return res.json({
+          message: "Maximum number of images for this resource was reached",
+          statusCode: 403,
+        });
+    }
+    
+
+    
+    const image = await Image.create({
+        url,
+        previewImage,
+        userId: user.id,
+        reviewId: req.params.reviewId,
+        spotId: null
+    })
+
+    let response = {
+        id: image.id,
+        imageableId: image.reviewId,
+        url: image.url
+    }
+
+    res.json(response)
+})
 
 
 
