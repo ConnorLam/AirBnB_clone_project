bda0e9a4d8c1e402be9c18b08583936bb4df062f
Connor Lam <connorlam03@yahoo.com>
2022-08-19 00:29:15 -0700

    login form i think?

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import {login} from "../../store/session";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import './LoginForm.css'
+
+
+
+const LoginFormPage = () => {
+
+    const history = useHistory()
+    const dispatch = useDispatch()
+    const user = useSelector(state => state.session.user)
+
+    const [credential, setCredential] = useState("");
+    const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState([])
+
+    if(user) {
+        history.push('/')
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        setErrors([]);
+        return dispatch(login({ credential, password }))
+        .catch(async (res) => {
+            const data = await res.json();
+            console.log(data)
+            if (data && data.errors) setErrors(data.errors);
+          }
+        );
+    }
+
+  return(
+    <form onSubmit={onSubmit}>
+        <ul>
+            {errors.map((error) => <li key={error}>{error}</li>)}
+        </ul>
+        <label>
+            Username or Email
+            <input type='text' onChange={e => setCredential(e.target.value)} required />
+        </label>
+        <label>
+            Password
+            <input type='text' onChange={e => setPassword(e.target.value)} required />
+        </label>
+    <button type="submit">Log In</button>
+    </form>
+  );
+};
+
+export default LoginFormPage
