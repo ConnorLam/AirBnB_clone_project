1d9f2ce6166812d623c6e27a23aef734e320976d
Connor Lam <connorlam03@yahoo.com>
2022-08-18 23:46:58 -0700

    i think finish with login

---
+++ frontend/src/store/session.js
@@ -0,0 +1,63 @@
+import { csrfFetch } from "./csrf";
+
+const SET_USER = "session/setUser";
+const DELETE_USER = "session/deleteUser";
+
+
+
+
+const setUser = (user) => {
+  return {
+    type: SET_USER,
+    user,
+  };
+};
+
+const removeUser = (user) => {
+  return {
+    type: DELETE_USER,
+  };
+};
+
+
+
+
+export const login = (user) => async (dispatch) => {
+    const {credential, password} = user
+    const res = await csrfFetch('/api/session', {
+        method: 'POST',
+        body: JSON.stringify({
+            credential, password
+        })
+    })
+    console.log('this is my response', res)
+    if (res.ok){
+        const data = await res.json()
+        console.log('this is my data', data)
+        dispatch(setUser(data))
+    }
+}
+
+
+
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case SET_USER:
+      newState = { ...state };
+      newState.user = action.user;
+      return newState;
+    case DELETE_USER:
+      newState = { ...state };
+      newState.user = null;
+      // delete newState['user']
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
