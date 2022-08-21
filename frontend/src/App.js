f3054426e3682c6a028e46ac355b599fe5b434ca
Connor Lam <connorlam03@yahoo.com>
2022-08-20 18:42:29 -0700

    typo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import { restoreUser } from './store/session';
 import {Route, Switch} from 'react-router-dom'
 // import LoginFormPage from './components/LoginFormModal';
-import SignupFormPage from './components/SingupFormPage';
+import SignupFormPage from './components/SignupFormPage';
 import {useDispatch} from 'react-redux'
 import Navigation from './components/Navigation'
 
